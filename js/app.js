64cb679f04069d7da2e65e3cca6dfc3464260b06
dules12 <danielgeo@comcast.net>
2018-06-05 17:01:10 -0700

    First question completed by allowing a 'no' or 'No' response to give a correct repsonse

---
+++ js/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+
+alert('Learn more about me by answering these five yes or no questions!');
+var born = prompt('Was I born on the west coast of the US?').toLowerCase();
+if (born === 'no') {
+   alert('Yes that\'s correct! Dan was born in New York');
+} 
+else {
+    alert('Sorry I guess you don\'t really know Dan :/');
+}
+console.log('Q: Was I born on the west coast of the US? A: No');
