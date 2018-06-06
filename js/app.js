90176951a02ad5259b876f0bd716abd73dde093c
dules12 <danielgeo@comcast.net>
2018-06-05 17:31:13 -0700

    Five questions have been asked in the same format and console logs added

--- js/app.js
+++ js/app.js
@@ -11,3 +11,42 @@ else {
     alert('Sorry I guess you don\'t really know Dan :/');
 }
 console.log('Q: Was I born on the west coast of the US? A: No');
+
+var school = prompt('Did I major in economics in college?').toLowerCase();
+if (school === 'no') {
+   alert('Yes that\'s correct! Dan majored in finance');
+} 
+else {
+    alert('Sorry I guess you need to go back to school');
+}
+console.log('Q: Did I major in economics in college? A: No');
+
+var language = prompt('Did I study Mandarin Chinese in school?').toLowerCase();
+if (language === 'yes') {
+   alert('Yes that\'s correct! Dan studied Mandarin Chinese for a couple years');
+} 
+else {
+    alert('Wrong! You thought he studied Spanish didn\'t you?');
+}
+console.log('Q: Did I study Mandarin Chinese in school A: Yes');
+
+var travel = prompt('Have I traveled to South Africa?').toLowerCase();
+if (travel === 'no') {
+   alert('Yes that\'s correct! Unfortunately he hasn\'t had the opportunity to travel there yet');
+} 
+else {
+    alert('That\'s not the answer we were looking for');
+}
+console.log('Q: Have I traveled to South Africa A: No');
+
+var food = prompt('Is pizza my favorite food?').toLowerCase();
+if (food === 'yes') {
+   alert('Yes that\'s correct! Pizza = yum');
+} 
+else {
+    alert('Sorry... contrary to popular belief it\'s not broccoli');
+}
+console.log('Q: Is pizza my favroite food A: Yes');
+
+
+
